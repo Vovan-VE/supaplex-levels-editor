0b4a0391476b4d52a6dccfc1d198cc2d05389cc8
Vovan-VE <vovan-ve@yandex.ru>
2022-08-04 17:15:25 +0800

    Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
