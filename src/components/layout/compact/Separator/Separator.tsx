c7adc7e552ccc4d7d7ce8f458bdf83bfeafff181
Vovan-VE <vovan-ve@yandex.ru>
2022-10-23 16:29:34 +0800

    Unused UI component

--- src/components/layout/compact/Separator/Separator.tsx
+++
@@ -1,6 +0,0 @@
-import { FC, PropsWithChildren } from "react";
-import cl from "./Separator.module.scss";
-
-export const Separator: FC<PropsWithChildren<{}>> = ({ children }) => (
-  <div className={cl.root}>{children}</div>
-);
