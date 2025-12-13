e45c443b6d0295c6f34d7533b42551ea2915a844
Vovan-VE <vovan-ve@yandex.ru>
2025-12-13 16:52:15 +0800

    Refact: del useless test with deprecated package

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer";
-import { App } from "./App";
-
-test("renders learn react link", () => {
-  const r = renderer.create(<App />);
-  r.unmount();
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
