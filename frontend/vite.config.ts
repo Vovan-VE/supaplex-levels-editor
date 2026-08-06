e311b0538a6c6dc89bee6329a97f086d7f2b0164
Vovan-VE <vovan-ve@yandex.ru>
2026-08-06 15:27:00 +0800

    Upgrade vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react({}), svgr()],
+  plugins: [react(), svgr()],
 
   build: {
     sourcemap: true,
+    // cssMinify: "esbuild",
   },
   css: {
+    // transformer: "postcss",
     preprocessorOptions: {
       scss: {
         loadPaths: ["./src/"],
       },
     },
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 });
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest/config" />
 
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     environment: "happy-dom",
     deps: {
       moduleDirectories: ["node_modules", "src"],
     },
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 });
